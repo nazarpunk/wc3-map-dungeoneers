ddedd281e112ce1cf5acdf246022bc533189a676
nazarpunk <nazarpunk@gmail.com>
2023-01-21 05:49:12 +0200

    maze

--- web/maze/dungeon.js
+++ web/maze/dungeon.js
@@ -1,14 +1,20 @@
-let SEED, SQUARE;
-let MAP_SIZE = 50;
+let SEED;
+const CellWidth = 80;
+const CellHeight = 80;
+const CellSize = 14;
 
-const ctx = document.getElementById('viewport').getContext('2d');
+/** @type {HTMLCanvasElement} */
+const canvas = document.getElementById('viewport');
+const ctx = canvas.getContext('2d');
 
 const random = (min, max) => Math.floor(Math.random() * (max - min + 1) + min);
 
-const Point = function (x, y) {
-	this.x = x;
-	this.y = y
-};
+class Point {
+	constructor(x, y) {
+		this.x = x;
+		this.y = y
+	}
+}
 
 class Room {
 	constructor(x, y, w, h) {
@@ -21,16 +27,20 @@ class Room {
 
 	paint() {
 		ctx.fillStyle = "#888";
-		ctx.fillRect(this.x * SQUARE, this.y * SQUARE,
-			this.w * SQUARE, this.h * SQUARE)
+		ctx.fillRect(
+			this.x * CellSize,
+			this.y * CellSize,
+			this.w * CellSize,
+			this.h * CellSize
+		)
 	}
 
 	drawPath(c, point) {
 		c.beginPath();
-		c.lineWidth = SQUARE;
+		c.lineWidth = CellSize;
 		c.strokeStyle = "#888";
-		c.moveTo(this.center.x * SQUARE, this.center.y * SQUARE);
-		c.lineTo(point.x * SQUARE, point.y * SQUARE);
+		c.moveTo(this.center.x * CellSize, this.center.y * CellSize);
+		c.lineTo(point.x * CellSize, point.y * CellSize);
 		c.stroke()
 	}
 }
@@ -44,68 +54,77 @@ class RoomContainer extends Room {
 	paint() {
 		ctx.strokeStyle = "#0F0";
 		ctx.lineWidth = 2;
-		ctx.strokeRect(this.x * SQUARE, this.y * SQUARE,
-			this.w * SQUARE, this.h * SQUARE)
+		ctx.strokeRect(this.x * CellSize, this.y * CellSize,
+			this.w * CellSize, this.h * CellSize)
 	}
 
 	growRoom() {
 		let x, y, w, h;
-		x = this.x + random(0, Math.floor(this.w / 3));
-		y = this.y + random(0, Math.floor(this.h / 3));
-		w = this.w - (x - this.x);
-		h = this.h - (y - this.y);
-		w -= random(0, w / 3);
-		h -= random(0, h / 3);
-		this.room = new Room(x, y, w, h)
+		if (0) {
+			x = this.x + random(0, Math.floor(this.w / 3));
+			y = this.y + random(0, Math.floor(this.h / 3));
+			w = this.w - (x - this.x);
+			h = this.h - (y - this.y);
+			w -= random(0, w / 3);
+			h -= random(0, h / 3);
+			this.room = new Room(x, y, w, h);
+		} else {
+			this.room = new Room(this.x + 1, this.y + 1, this.w - 2, this.h - 2);
+		}
 	}
 }
 
 
-function random_split(room) {
+function randomSplit(room) {
 	let r1, r2;
 	if (random(0, 1) === 0) {
 		// Vertical
-		r1 = new RoomContainer(
-			room.x, room.y,
-			random(1, room.w), room.h,
-		);
-		r2 = new RoomContainer(
-			room.x + r1.w, room.y,
-			room.w - r1.w, room.h
-		);
+		r1 = new RoomContainer(room.x, room.y, random(1, room.w), room.h);
+		r2 = new RoomContainer(room.x + r1.w, room.y, room.w - r1.w, room.h);
 
 		if (r1.w / r1.h < .45 || r2.w / r2.h < .45) {
-			return random_split(room)
+			return randomSplit(room)
 		}
 	} else {
 		// Horizontal
-		r1 = new RoomContainer(
-			room.x, room.y,
-			room.w, random(1, room.h)
-		);
-		r2 = new RoomContainer(
-			room.x, room.y + r1.h,
-			room.w, room.h - r1.h
-		);
+		r1 = new RoomContainer(room.x, room.y, room.w, random(1, room.h));
+		r2 = new RoomContainer(room.x, room.y + r1.h, room.w, room.h - r1.h);
 
 		if (r1.h / r1.w < .45 || r2.h / r2.w < .45) {
-			return random_split(room)
+			return randomSplit(room)
 		}
 
 	}
 	return [r1, r2]
 }
 
-function split_room(room, iter) {
+function splitRoom(room, iter) {
 	const Root = new Tree(room);
 	room.paint();
 	if (iter !== 0) {
-		const sr = random_split(room);
-		Root.lchild = split_room(sr[0], iter - 1);
-		Root.rchild = split_room(sr[1], iter - 1)
+		const sr = randomSplit(room);
+		Root.lchild = splitRoom(sr[0], iter - 1);
+		Root.rchild = splitRoom(sr[1], iter - 1)
 	}
 	return Root
 }
+
+const drawGrid = () => {
+	ctx.beginPath();
+	ctx.strokeStyle = "rgba(255,255,255,0.4)";
+	ctx.lineWidth = 0.5;
+
+	for (let i = 0; i < CellWidth; i++) {
+		ctx.moveTo(i * CellSize, 0);
+		ctx.lineTo(i * CellSize, CellHeight * CellSize);
+	}
+	for (let i = 0; i < CellHeight; i++) {
+		ctx.moveTo(0, i * CellSize);
+		ctx.lineTo(CellWidth * CellSize, i * CellSize)
+	}
+	ctx.stroke();
+	ctx.closePath();
+};
 
 class Tree {
 	constructor(leaf) {
@@ -115,29 +134,34 @@ class Tree {
 	}
 
 	getLeafs() {
-		if (this.lchild === undefined && this.rchild === undefined)
+		if (this.lchild === undefined && this.rchild === undefined) {
 			return [this.leaf];
-		else
-			return [].concat(this.lchild.getLeafs(), this.rchild.getLeafs())
+		} else {
+			return [].concat(this.lchild.getLeafs(), this.rchild.getLeafs());
+		}
 	}
 
 	paint() {
 		this.leaf.paint();
-		if (this.lchild !== undefined)
+		if (this.lchild !== undefined) {
 			this.lchild.paint();
-		if (this.rchild !== undefined)
-			this.rchild.paint()
+		}
+		if (this.rchild !== undefined) {
+			this.rchild.paint();
+		}
 	}
 }
 
 class GameMap {
 	constructor() {
-		this.width = SQUARE * MAP_SIZE;
-		this.height = SQUARE * MAP_SIZE;
+		this.width = CellSize * CellWidth;
+		this.height = CellSize * CellHeight;
+		canvas.width = this.width;
+		canvas.height = this.height;
 		this.rooms = [];
-		const main_room = new RoomContainer(0, 0, MAP_SIZE, MAP_SIZE);
-		this.room_tree = split_room(main_room, 2);
-		const leafs = this.room_tree.getLeafs();
+		const mainRoom = new RoomContainer(0, 0, CellWidth, CellHeight);
+		this.roomTree = splitRoom(mainRoom, 2);
+		const leafs = this.roomTree.getLeafs();
 		for (let i = 0; i < leafs.length; i++) {
 			leafs[i].growRoom();
 			this.rooms.push(leafs[i].room)
@@ -146,42 +170,27 @@ class GameMap {
 
 	clear() {
 		ctx.fillStyle = "#000";
-		ctx.fillRect(0, 0, this.width, this.height)
+		ctx.fillRect(0, 0, this.width, this.height);
 	}
 
 	drawPaths(tree) {
 		if (tree.lchild !== undefined && tree.rchild !== undefined) {
 			tree.lchild.leaf.drawPath(ctx, tree.rchild.leaf.center);
 			this.drawPaths(tree.lchild);
-			this.drawPaths(tree.rchild)
+			this.drawPaths(tree.rchild);
 		}
-	}
-
-	drawGrid() {
-		ctx.beginPath();
-		ctx.strokeStyle = "rgba(255,255,255,0.4)";
-		ctx.lineWidth = 0.5;
-		for (let i = 0; i < MAP_SIZE; i++) {
-			ctx.moveTo(i * SQUARE, 0);
-			ctx.lineTo(i * SQUARE, MAP_SIZE * SQUARE);
-			ctx.moveTo(0, i * SQUARE);
-			ctx.lineTo(MAP_SIZE * SQUARE, i * SQUARE)
-		}
-		ctx.stroke();
-		ctx.closePath()
 	}
 
 	paint() {
 		this.clear();
-		this.room_tree.paint();
+		this.roomTree.paint();
 
 		for (let i = 0; i < this.rooms.length; i++) {
 			this.rooms[i].paint()
 		}
 
-
-		this.drawPaths(this.room_tree);
-		this.drawGrid()
+		this.drawPaths(this.roomTree);
+		drawGrid()
 	}
 }
 
@@ -189,7 +198,6 @@ const initMap = seed => {
 	try {
 		SEED = seed ?? CryptoJS.MD5(`${new Date().getTime()}`).toString();
 		document.getElementById('seed').value = SEED;
-		SQUARE = 400 / MAP_SIZE;
 		Math.seedrandom(SEED);
 		(new GameMap()).paint();
 	} catch (exception) {
