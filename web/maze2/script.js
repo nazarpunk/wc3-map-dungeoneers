c3463d557caf911007c819e272e23e91fdfc45c1
nazarpunk <nazarpunk@gmail.com>
2023-01-27 01:41:39 +0200

    map

--- web/maze2/script.js
+++ web/maze2/script.js
@@ -10,9 +10,7 @@ class Random {
 	next() {
 		this.m_z = 36969 * (this.m_z & 65535) + (this.m_z >> 16) & this.mask;
 		this.m_w = 18000 * (this.m_w & 65535) + (this.m_w >> 16) & this.mask;
-		let result = (this.m_z << 16) + this.m_w & this.mask;
-		result /= 4294967296;
-		return result + 0.5;
+		return ((this.m_z << 16) + this.m_w & this.mask) / 4294967296 + .5;
 	}
 }
 
